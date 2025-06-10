6a165b2b40b7b2598e9aba84994be7664e4182a1
kimheejoo0828 <o1043381828@nate.com>
2025-06-10 21:11:14 +0900

    방어구추가
    방패 1개 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,19 +23,18 @@ public class GameManager : MonoBehaviour
     private void SetData()
     {
         Sprite swordIcon = Resources.Load<Sprite>("Icons/sword");
-        Sprite armorIcon = Resources.Load<Sprite>("Icons/armor");
+        Sprite shieldIcon = Resources.Load<Sprite>("Icons/shield");
         Sprite axeIcon = Resources.Load<Sprite>("Icons/axe");
+        Sprite ironshieldIcon = Resources.Load<Sprite>("Icons/ironshield");
 
         var sword = new Item("철검", "기본적인 검입니다.", swordIcon, ItemType.Weapon, attack: 5);
-        var armor = new Item("기본방패", "기본 방어구입니다.", armorIcon, ItemType.Armor, defense: 3);
+        var shield = new Item("기본방패", "기본 방어구입니다.", shieldIcon, ItemType.Armor, defense: 3);
         var axe = new Item("도끼", "기본적인 도끼입니다.", axeIcon, ItemType.Weapon, attack: 7);
+        var ironshield = new Item("철방패", "철로 만든 방패입니다.", ironshieldIcon, ItemType.Armor, defense: 8);
 
-        var inventory = new List<Item> { sword, armor, axe };
+        var inventory = new List<Item> { sword, shield, axe, ironshield };
 
         Player = new Character("Chad", 5, 10, 5, 100, 10, 2000, inventory);
-
-
-        Player.EquipItem(axe);
 
         UIManager.Instance.MainMenu.InitUI(Player);
         UIManager.Instance.Status.InitUI(Player);
